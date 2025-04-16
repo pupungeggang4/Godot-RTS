200546543321896092c8e2d6b7b9b4524df98b23
pupungeggang6 <pupungeggang6@gmail.com>
2025-04-16 14:00:53 +0900

    Start

---
+++ Game/Script/rts.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var node_unit = get_node('Field/Unit')
+
+func _ready() -> void:
+    var node_unit_temp = load('res://Scene/Thing/unit.tscn').instantiate()
+    node_unit_temp.position = Vector2(-800, 0)
+    node_unit_temp.get_node('Sprite').texture = load('res://Image/Base.png')
+    node_unit.add_child(node_unit_temp)
+    node_unit_temp = load('res://Scene/Thing/unit.tscn').instantiate()
+    node_unit_temp.position = Vector2(800, 0)
+    node_unit_temp.get_node('Sprite').texture = load('res://Image/Base2.png')
+    node_unit.add_child(node_unit_temp)
+    
+func _process(delta: float) -> void:
+    pass
---
+++ Game/Script/unit.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _ready() -> void:
+    pass
+    
+func set_texture() -> void:
+    pass
+    
+func _process(delta: float) -> void:
+    pass
